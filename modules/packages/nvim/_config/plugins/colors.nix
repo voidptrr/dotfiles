50189f6830ccb0e6dcbf335005cfb516dcb14123
voidptrr <bruno.tommaso@protonmail.com>
2026-02-16 20:04:38 +0000

    chore: move to everforest colorscheme

--- modules/packages/nvim/_config/plugins/colors.nix
+++ modules/packages/nvim/_config/plugins/colors.nix
@@ -1,26 +1,9 @@
-{pkgs, ...}: let
-  koda = pkgs.vimUtils.buildVimPlugin {
-    pname = "koda.nvim";
-    version = "main";
-    src = pkgs.fetchFromGitHub {
-      owner = "oskarnurm";
-      repo = "koda.nvim";
-      rev = "main";
-      sha256 = "sha256-kErR1LB4K/RAFKcSUV/VnQajtGVFj+0rdvNn84hWpUY=";
+{
+  colorschemes.everforest = {
+    enable = true;
+    settings = {
+      background = "hard";
+      transparent_background = 1;
     };
   };
-in {
-  extraPlugins = [koda];
-  extraConfigLua = ''
-    require("koda").setup({
-      styles = {
-        functions = { bold = true, italic = false },
-        keywords = { italic = false },
-        comments = { italic = false },
-        strings = { italic = false },
-        constants = { italic = false },
-      },
-    })
-    vim.cmd.colorscheme("koda")
-  '';
 }
--- modules/terminal/ghostty.nix
+++ modules/terminal/ghostty.nix
@@ -14,7 +14,8 @@
         font-thicken-strength = 100;
         font-family = "Source Code Pro";
 
-        background = "#0d0d0d";
+        theme = "Everforest Dark Hard";
+
         window-width = 144;
         window-height = 43;
       };
