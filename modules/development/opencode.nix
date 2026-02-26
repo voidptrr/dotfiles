b23d619d4978bf43f0fdc56eb7d3ee2325f3e5aa
voidptrr <bruno.tommaso@protonmail.com>
2026-02-26 23:17:24 +0000

    chore: remove non needed plugin for openai codex

--- modules/development/opencode.nix
+++ modules/development/opencode.nix
@@ -3,54 +3,9 @@
     programs.opencode = {
       enable = true;
       settings = {
-        plugin = ["opencode-openai-codex-auth"];
-        theme = "opencode";
-        provider = {
-          openai = {
-            options = {
-              reasoningEffort = "medium";
-              reasoningSummary = "auto";
-              textVerbosity = "medium";
-              include = ["reasoning.encrypted_content"];
-              store = false;
-            };
-            models = {
-              "gpt-5.2-codex" = {
-                name = "GPT 5.2 Codex (OAuth)";
-                limit = {
-                  context = 272000;
-                  output = 128000;
-                };
-                modalities = {
-                  input = ["text" "image"];
-                  output = ["text"];
-                };
-                variants = {
-                  low = {
-                    reasoningEffort = "low";
-                    reasoningSummary = "auto";
-                    textVerbosity = "medium";
-                  };
-                  medium = {
-                    reasoningEffort = "medium";
-                    reasoningSummary = "auto";
-                    textVerbosity = "medium";
-                  };
-                  high = {
-                    reasoningEffort = "high";
-                    reasoningSummary = "detailed";
-                    textVerbosity = "medium";
-                  };
-                  xhigh = {
-                    reasoningEffort = "xhigh";
-                    reasoningSummary = "detailed";
-                    textVerbosity = "medium";
-                  };
-                };
-              };
-            };
-          };
-        };
+        plugin = [];
+        theme = "gruvbox";
+        provider = {};
       };
     };
   };
