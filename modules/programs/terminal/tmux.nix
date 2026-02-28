c9f8395137a608e581c6c6c5dec8768b02fa6b40
voidptrr <bruno.tommaso@protonmail.com>
2026-02-28 19:05:42 +0000

    chore: keep gruvbox colors

--- modules/programs/terminal/tmux.nix
+++ modules/programs/terminal/tmux.nix
@@ -12,6 +12,13 @@
       shell = "${pkgs.zsh}/bin/zsh";
       plugins = [
         {
+          plugin = pkgs.tmuxPlugins.gruvbox;
+          extraConfig = ''
+            set -g @tmux-gruvbox 'dark'
+            set -g @tmux-gruvbox-statusbar-alpha 'true'
+          '';
+        }
+        {
           plugin = pkgs.tmuxPlugins.prefix-highlight;
           extraConfig = ''
             set -g @prefix_highlight_show_copy_mode 'on'
@@ -22,24 +29,25 @@
         }
         {
           plugin = pkgs.tmuxPlugins.fzf-tmux-url;
+          extraConfig = ''
+            set -g status on
+            set -g status-position bottom
+            set -g status-interval 5
+            set -g status-style "bg=default,fg=colour245"
+            set -g status-left ""
+            set -g status-right "#{prefix_highlight}"
+            set -g status-left-length 0
+            set -g status-right-length 40
+
+            set -g window-status-separator " "
+            set -g window-status-style "fg=colour245,bg=default"
+            set -g window-status-current-style "fg=colour235,bg=colour220,bold"
+            set -g window-status-format " #I:#W "
+            set -g window-status-current-format " #I:#W "
+          '';
         }
       ];
       extraConfig = ''
-        set -g status on
-        set -g status-position bottom
-        set -g status-interval 5
-        set -g status-style "bg=default,fg=colour245"
-        set -g status-left ""
-        set -g status-right "#{prefix_highlight}"
-        set -g status-left-length 0
-        set -g status-right-length 40
-
-        set -g window-status-separator " "
-        set -g window-status-style "fg=colour245,bg=default"
-        set -g window-status-current-style "fg=colour235,bg=colour220,bold"
-        set -g window-status-format " #I:#W "
-        set -g window-status-current-format " #I:#W "
-
         set-hook -g after-new-session 'if-shell -F "#{==:#{window_panes},1}" "split-window -h -c \"#{pane_current_path}\"; select-pane -L; resize-pane -R 15"'
 
         bind '"' split-window -c "#{pane_current_path}"
