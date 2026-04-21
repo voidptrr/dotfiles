d7ea4a602fdf9874fef985d50c149a6142627ddc
voidptrr <bruno.tommaso@protonmail.com>
2026-04-21 01:14:11 +0100

    cho

--- modules/hm/dev/nvim/theme.nix
+++ modules/hm/dev/nvim/theme.nix
@@ -3,9 +3,13 @@
   isKanagawa = osConfig.nixos.theme.enable && osConfig.nixos.theme.name == "kanagawa";
   c = {
     bg0 = p.bg0 or "#181616";
+    bg1 = p.bg1 or "#282727";
+    bg2 = p.bg2 or "#393836";
     fg1 = p.fg1 or "#c5c9c5";
+    fg2 = p.fg2 or "#a6a69c";
     red = p.red or "#c4746e";
     yellow = p.yellow or "#c4b28a";
+    blue = p.blue or "#8ba4b0";
     aqua = p.aqua or "#8ea4a2";
   };
   variant =
@@ -28,6 +32,36 @@ in {
     };
 
     highlight = {
+      Pmenu = {
+        bg = c.bg1;
+      };
+      PmenuSel = {
+        bg = c.bg2;
+        fg = c.fg1;
+      };
+      PmenuKind = {
+        bg = c.bg1;
+        fg = c.fg1;
+      };
+      PmenuKindSel = {
+        bg = c.bg2;
+        fg = c.fg1;
+      };
+      PmenuExtra = {
+        bg = c.bg1;
+        fg = c.fg2;
+      };
+      PmenuExtraSel = {
+        bg = c.bg2;
+        fg = c.fg2;
+      };
+      PmenuSbar = {
+        bg = c.bg0;
+      };
+      PmenuThumb = {
+        bg = c.blue;
+      };
+
       User1 = {
         bg = c.bg0;
         fg = c.aqua;
