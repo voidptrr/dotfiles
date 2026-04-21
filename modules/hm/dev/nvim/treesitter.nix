5558f4a2f84019868e9a4208854f178a764285ea
voidptrr <bruno.tommaso@protonmail.com>
2026-04-21 01:30:25 +0100

    chore: treesitter grammars

--- modules/hm/dev/nvim/treesitter.nix
+++ modules/hm/dev/nvim/treesitter.nix
@@ -1,8 +1,14 @@
-{...}: {
+{config, ...}: {
   programs.nixvim = {
     plugins.treesitter = {
       enable = true;
       folding.enable = true;
+
+      grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
+        rust
+        nix
+        c
+      ];
       settings = {
         highlight.enable = true;
         indent.enable = true;
